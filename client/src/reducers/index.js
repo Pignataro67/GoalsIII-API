37a112b48fea42159d41996750bd6d2f15c3f504
Pignataro67 <Pignataro67@gmail.com>
2020-05-06 11:28:53 -0700

    App.js and index.js setup

---
+++ client/src/reducers/index.js
@@ -0,0 +1,12 @@
+export default function rootReducer(state = {
+    goals: []
+  }, action = {type:"ADD_GOAL", goal:"objective1"}){
+  console.log("hitaction");
+  switch(action.type){
+    case 'ADD_GOAL':
+      console.log("hit reducer and first action")
+      break;
+    default:
+      console.log("hit default reducer statement")
+  }
+}

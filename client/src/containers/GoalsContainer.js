649c002f432279afff3fa65d22892b7645ef8901
Pignataro67 <Pignataro67@gmail.com>
2020-05-07 06:34:22 -0700

    adding cont./comp.

--- client/src/containers/GoalsContainer.js
+++ client/src/containers/GoalsContainer.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import AddGoal from '../components/Goals/AddGoal';
+import GoalList from '../components/Goals/GoalList';
 
 class GoalsContainer extends Component {
   render(){
     return(
       <div>
         <AddGoal />
+        <GoalList />
       </div>
     )
   }

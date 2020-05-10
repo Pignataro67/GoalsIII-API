3764e3bbfa1ece9d0a7563d4ca8cbf56560a435f
Pignataro67 <Pignataro67@gmail.com>
2020-05-10 07:21:17 -0700

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
 import GoalsContainer from './containers/GoalsContainer';
+import NavBar from './components/Navbar';
+import { Route } from 'react-router-dom';
+import AddGoal from './components/Goals/AddGoal';
 
 function App() {
   return (
     <div className="App">
       <h1>Goals</h1>
       <GoalsContainer />
+      <NavBar />
+        <div>
+          <Route exact path='/goals/new' component={AddGoal} />
+          <Route exact path='/goals' component={GoalsContainer} />
+        </div>
     </div>
   );
 }

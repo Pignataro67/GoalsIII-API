7e44834af005482a7ec013dbd0202d2dac14105d
Pignataro67 <Pignataro67@gmail.com>
2020-05-11 08:08:24 -0700

    containerizing comp.

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import './App.css';
-import GoalsContainer from './containers/GoalsContainer';
 import NavBar from './components/Navbar';
 import { Route } from 'react-router-dom';
 import Home from './components/Home';
 import AddGoal from './components/Goals/AddGoal';
 import GoalsContainer from './containers/GoalsContainer';
-import { Button } from 'semantic-ui-react';
+//Semantic-Ui-Components
+import { Container } from 'semantic-ui-react';
+import { Header } from 'semantic-ui-react';
 
 class App extends Component {
   render() {	 
   return (
-    <div className="App">
-      <h1>Goals</h1>
-      <p>Stay Focused</p>
-      <Button>Click Me</Button>
+    <Container>
+    <Header as='h1' className="main-header">Goals</Header>
+      <Header as='h3'className="tagline">TagLine here</Header>
       <NavBar />
         <div>
-          <Route exact path='/' render={Home} />
-          <Route exact path='/goals/new' component={AddGoal} />
-          <Route exact path='/goals' component={GoalsContainer} />
+          <Container>
+            <Route exact path='/' render={Home} />
+            <Route exact path='/goals/new' component={AddGoal} />
+            <Route exact path='/goals' component={GoalsContainer} />
+          </Container>
         </div>
-    </div>
+    </Container>
     );
   }
 }

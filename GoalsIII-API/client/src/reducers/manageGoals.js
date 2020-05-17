b7a85c4beb8bb56dc857d3ad1701c6671afd7021
Pignataro67 <Pignataro67@gmail.com>
2020-05-17 07:46:00 -0700

    adding reducer

--- GoalsIII-API/client/src/reducers/manageGoals.js
+++ GoalsIII-API/client/src/reducers/manageGoals.js
@@ -4,6 +4,12 @@ export default function goalsReducer(state = {
   switch(action.type){
     case 'ADD_GOAL':
       console.log('adding', action.goal )
+    case 'LOADING_GOALS':
+      console.log("loading goals now")
+      return {...state, loading: [...state.loading, true]}
+    case 'FETCH_GOALS':
+      console.log("fetch goals")
+      return {loading: false, goals: action.payload}
     return [
         ...state,
         action.goal]
